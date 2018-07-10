482ff1f94c1b1d8d9e6b01dc620d2ac26fce5915
hadadach <hadadach@bowdoin.edu>
2018-07-10 14:47:28 -0700

    configuring enviornment

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+var config = {
+    entry: './frontend/javascripts/App.js',
+    output: {
+       path: path.resolve(__dirname, "build"),
+       filename: 'bundle.js',
+    },
+    devServer: {
+       inline: true,
+       port: 8080
+    },
+    module: {
+       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader','css-loader'],
+          include: [/frontend/, /node_modules/]
+        },{
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        }
+       ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+        template: "./frontend/index.html",
+        filename: "./index.html"
+      })
+    ]
+ }
+ module.exports = config;
